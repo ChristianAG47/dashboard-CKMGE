e48a2141bd4071091bc182be5d64b60013c01d10
ChristianAG47 <sebastian_97_cristian@hotmail.com>
2022-11-07 00:03:55 -0600

    Actualizado
    Nombres correctos

--- pages/rtl-edit2.php
+++ pages/rtl-edit2.php
@@ -7,7 +7,7 @@
   inner join productos ON detallemant.id_producto=productos.id_producto*/
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="utf-8" />
@@ -56,7 +56,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="../pages/tables.php">
+          <a class="nav-link " href="../pages/tables.php">
             <div
               class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
@@ -83,7 +83,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="../pages/rtl-edit2.php">
+          <a class="nav-link active" href="../pages/rtl-edit2.php">
             <div
               class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
@@ -102,20 +102,6 @@
         </li>
       </ul>
     </div>
-    <div class="sidenav-footer mx-3 ">
-      <div class="card card-plain shadow-none" id="sidenavCard">
-        <img class="w-50 mx-auto" src="../assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
-        <div class="card-body text-center p-3 w-100 pt-0">
-          <div class="docs-info">
-            <h6 class="mb-0">Need help?</h6>
-            <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
-          </div>
-        </div>
-      </div>
-      <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard" target="_blank"
-        class="btn btn-dark btn-sm w-100 mb-3">Documentation
-      </a>
-    </div>
   </aside>
   <main class="main-content position-relative border-radius-lg ">
     <!-- Navbar -->
@@ -125,38 +111,39 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Página</a></li>
-            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Calendario</li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Registro de Mantenimientos</li>
           </ol>
           <h6 class="font-weight-bolder text-white mb-0">Mantenimientos</h6>
         </nav>
+        <!-- Fin Encabezado de Pagina -->
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <!-- Inicio Buscador -->
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            <!--
             <div class="input-group">
               <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
               <input type="text" class="form-control" placeholder="Type here...">
             </div>
+            -->
           </div>
+          <!-- Fin Buscador -->
           <ul class="navbar-nav  justify-content-end">
+            <!-- Inicio LogIn -->
             <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+              <a href="sign-in.php" class="nav-link text-white font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
                 <span class="d-sm-inline d-none">Sign In</span>
               </a>
             </li>
-            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line bg-white"></i>
-                  <i class="sidenav-toggler-line bg-white"></i>
-                  <i class="sidenav-toggler-line bg-white"></i>
-                </div>
-              </a>
-            </li>
+            <!-- Fin LogIn -->
+            <!-- Inicio Config -->
             <li class="nav-item px-3 d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-white p-0">
                 <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
               </a>
             </li>
+            <!-- Fin Config -->
+            <!-- Inicio Alertas -->
             <li class="nav-item dropdown pe-2 d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton" data-bs-toggle="dropdown"
                 aria-expanded="false">
@@ -173,145 +160,69 @@
                         <h6 class="text-sm font-weight-normal mb-1">
                           <span class="font-weight-bold">New message</span> from Laur
                         </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          13 minutes ago
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li class="mb-2">
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="my-auto">
-                        <img src="../assets/img/small-logos/logo-spotify.svg"
-                          class="avatar avatar-sm bg-gradient-dark  me-3 ">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New album</span> by Travis Scott
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          1 day
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">
-                        <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
-                          xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                          <title>credit-card</title>
-                          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                              <g transform="translate(1716.000000, 291.000000)">
-                                <g transform="translate(453.000000, 454.000000)">
-                                  <path class="color-background"
-                                    d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-                                    opacity="0.593633743"></path>
-                                  <path class="color-background"
-                                    d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-                                  </path>
-                                </g>
-                              </g>
-                            </g>
-                          </g>
-                        </svg>
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          Payment successfully completed
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          2 days
-                        </p>
                       </div>
                     </div>
                   </a>
                 </li>
               </ul>
             </li>
+            <!-- Fin Alertas -->
           </ul>
         </div>
       </div>
     </nav>
     <!-- End Navbar -->
+
     <div class="container-fluid py-4">
       <div class="row">
         <div class="col-12">
-          <div class="card mb-4">
-            <div class="card-header pb-0">
-              <h6>Tabla de Direcciones</h6>
-            </div>
-            <div class="card-body px-0 pt-0 pb-2">
-              <div class="table-responsive p-0">
-                <table class="table align-items-center mb-0">
-                  <thead>
-                    <tr>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Cliente</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Direccion
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Telefono</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Fecha</th>
-                      </th>
-                      <th class="text-secondary opacity-7"></th>
-                    </tr>
-                  </thead>
-                  <div class="col-md-14 mt-4">
-                    <div class="card">
-                      <div class="card-header pb-0 px-3">
-                        <h6 class="mb-0">Registro de Mantenimientos</h6>
-                      </div>
-                      <div class="card-body pt-4 p-3">
-                        <form action="regmant.php" name="form" method="POST" id="form">
+          <div class="col-md-14 mt-4">
+            <div class="card">
+              <div class="card-header pb-0 px-3">
+                <h6 class="mb-0">Registro de Mantenimientos</h6>
+              </div>
+              <div class="card-body pt-4 p-3">
+                <form action="regmant.php" name="form" method="POST" id="form">
 
-                        <div class="form-group">
-                            <!-- Conexion con tabla Productos -->
-                            <label for="" class="control-label">Producto</label>
-                            <select class="form-control" name="idProductos" id= "idProductos">
-                              <option  value="">Seleccione el Producto</option>
-                              <?php 
+                  <div class="form-group">
+                    <!-- Conexion con tabla Productos -->
+                    <label for="" class="control-label">Producto</label>
+                    <select class="form-control" name="idProductos" id="idProductos">
+                      <option value="">Seleccione el Producto</option>
+                      <?php 
                                 $sql=$mysqli->query("select * from productos");
                                   while ($fila=$sql->fetch_array()) {
                                     echo "<option value='".$fila['id_producto']."'>".$fila['marca_producto']."</option>";
                                   }
                               ?>
-                            </select>
-                          </div>
+                    </select>
+                  </div>
 
-                          <div class="form-group">
-                            <!-- Fecha mantenimientos -->
-                            <label for="" class="control-label">Fecha Mantenimiento</label>
-                            <input class="form-control text-secondary text-xs font-weight-bold" type="date" id="fechamant" name="fechamant" value="0000-00-00" min="2022-11-25" max="2024-01-31">
-                          </div>
+                  <div class="form-group">
+                    <!-- Fecha mantenimientos -->
+                    <label for="" class="control-label">Fecha Mantenimiento</label>
+                    <input class="form-control text-secondary text-xs font-weight-bold" type="date" id="fechamant"
+                      name="fechamant" value="" min="2022-01-11" max="2024-01-31" onfocus="this.min=new Date().toISOString().split('T')[0]">
+                  </div>
 
-                          <div class="form-group">
-                            <!-- Conexion con tabla Clientes -->
-                            <label for="" class="control-label">Cliente</label>
-                            <select class="form-control" name="idClientes" id= "idClientes">
-                              <option  value="">Seleccione el Cliente</option>
-                              <?php 
+                  <div class="form-group">
+                    <!-- Conexion con tabla Clientes -->
+                    <label for="" class="control-label">Cliente</label>
+                    <select class="form-control" name="idClientes" id="idClientes">
+                      <option value="">Seleccione el Cliente</option>
+                      <?php 
                                 $sql=$mysqli->query("select * from clientes");
                                   while ($fila=$sql->fetch_array()) {
                                     echo "<option value='".$fila['id_Clientes']."'>".$fila['nom_clientes']."</option>";
                                   }
                               ?>
-                            </select>
-                          </div>
-                          <div class="form-group">
-                            <!-- Submit Button -->
-                            <input type="submit" name="regmant" value="Registrar" class="btn btn-primary" href="tables.php">
-                          </div>
-                        </form>
-                      </div>
-                    </div>
+                    </select>
                   </div>
-                </table>
+                  <div class="form-group">
+                    <!-- Submit Button -->
+                    <input type="submit" name="regmant" value="Registrar" class="btn btn-primary" href="tables.php">
+                  </div>
+                </form>
               </div>
             </div>
           </div>
@@ -326,8 +237,8 @@
     <div class="card shadow-lg">
       <div class="card-header pb-0 pt-3 ">
         <div class="float-start">
-          <h5 class="mt-3 mb-0">Argon Configurator</h5>
-          <p>See our dashboard options.</p>
+          <h5 class="mt-3 mb-0">Configuración dashboard</h5>
+          <p>Opciones de visualización/dashboard.</p>
         </div>
         <div class="float-end mt-4">
           <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
@@ -355,8 +266,8 @@
         </a>
         <!-- Sidenav Type -->
         <div class="mt-3">
-          <h6 class="mb-0">Sidenav Type</h6>
-          <p class="text-sm">Choose between 2 different sidenav types.</p>
+          <h6 class="mb-0">Barra de Navegación Lateral</h6>
+          <p class="text-sm">Elige entre los 2 diferentes tipos de presentación.</p>
         </div>
         <div class="d-flex">
           <button class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2" data-class="bg-white"
@@ -378,24 +289,6 @@
           <div class="form-check form-switch ps-0 ms-auto my-auto">
             <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
           </div>
-        </div>
-        <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/argon-dashboard">Free
-          Download</a>
-        <a class="btn btn-outline-dark w-100"
-          href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard">View documentation</a>
-        <div class="w-100 text-center">
-          <a class="github-button" href="https://github.com/creativetimofficial/argon-dashboard"
-            data-icon="octicon-star" data-size="large" data-show-count="true"
-            aria-label="Star creativetimofficial/argon-dashboard on GitHub">Star</a>
-          <h6 class="mt-3">Thank you for sharing!</h6>
-          <a href="https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard"
-            class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-          </a>
-          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard"
-            class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-          </a>
         </div>
       </div>
     </div>
